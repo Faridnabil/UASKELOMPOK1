423da50a03fc87e5bc42bdfc539183cd207013b9
Muhamad Azis <muhamadazis9a@gmail.com>
2022-01-23 23:31:54 +0700

    Rubah nama

--- Dashboard/ReportViewer.Designer.cs
+++ Dashboard/ReportViewer.Designer.cs
@@ -36,19 +36,20 @@ namespace Dashboard
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(383, 259);
+            this.label1.Location = new System.Drawing.Point(400, 178);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(487, 51);
+            this.label1.Size = new System.Drawing.Size(92, 39);
             this.label1.TabIndex = 0;
-            this.label1.Text = "FARID NABIL FIRDAUS";
+            this.label1.Text = "Fadil";
             // 
             // ReportViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1207, 641);
+            this.ClientSize = new System.Drawing.Size(905, 521);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ReportViewer";
             this.Text = "ReportViewer";
             this.ResumeLayout(false);
