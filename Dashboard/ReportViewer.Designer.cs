b10c4867417545660d5b594effd24f6e38a07673
Farid Nabil Firdaus <92703687+Faridnabil@users.noreply.github.com>
2022-01-23 22:36:38 +0700

    ReportViewer

--- Dashboard/ReportViewer.Designer.cs
+++ Dashboard/ReportViewer.Designer.cs
@@ -29,12 +29,34 @@ namespace Dashboard
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(336, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(498, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FARID NABIL FIRDAUS";
+            // 
+            // ReportViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1207, 641);
+            this.Controls.Add(this.label1);
+            this.Name = "ReportViewer";
             this.Text = "ReportViewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
