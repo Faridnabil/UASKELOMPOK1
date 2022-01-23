a87fc5afe6593d0238e3562c22edf35aed4604e8
Farid Nabil Firdaus <92703687+Faridnabil@users.noreply.github.com>
2022-01-23 22:57:29 +0700

    CrystalReport Ganti nama

--- Dashboard/ReportViewer.Designer.cs
+++ Dashboard/ReportViewer.Designer.cs
@@ -36,11 +36,11 @@ namespace Dashboard
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(336, 258);
+            this.label1.Location = new System.Drawing.Point(454, 278);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(498, 52);
+            this.label1.Size = new System.Drawing.Size(180, 51);
             this.label1.TabIndex = 0;
-            this.label1.Text = "FARID NABIL FIRDAUS";
+            this.label1.Text = "TRPL19";
             // 
             // ReportViewer
             // 
