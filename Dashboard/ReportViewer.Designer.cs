0f02603fb5644a2cb2dce0f54a100888a8eb85b7
Muhamad Azis <muhamadazis9a@gmail.com>
2022-01-23 23:00:07 +0700

    Perubahan lagi

--- Dashboard/ReportViewer.Designer.cs
+++ Dashboard/ReportViewer.Designer.cs
@@ -36,18 +36,20 @@ namespace Dashboard
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(454, 278);
+            this.label1.Location = new System.Drawing.Point(340, 226);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(180, 51);
+            this.label1.Size = new System.Drawing.Size(164, 39);
             this.label1.TabIndex = 0;
-            this.label1.Text = "TRPL19";
+            this.label1.Text = "Tah Farid";
             // 
             // ReportViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1207, 641);
+            this.ClientSize = new System.Drawing.Size(905, 521);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ReportViewer";
             this.Text = "ReportViewer";
             this.ResumeLayout(false);
